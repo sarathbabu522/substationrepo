de7857321039060f961c2e06852085c8e0f5b204
root <root@ip-172-31-63-239.ec2.internal>
2023-09-03 05:05:21 +0000

    build station

---
+++ src/main/java/powerstation/powerstation/PowerstationApplication.java
@@ -0,0 +1,13 @@
+package powerstation.powerstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerstationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/powerstation/powerstation/PowerstationApplicationTests.java
@@ -0,0 +1,13 @@
+package powerstation.powerstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
